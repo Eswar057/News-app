b84ed3d8e8f8165a5532d2fe6fdf2d6067c1cf3b
Eswar <84265712+Eswar057@users.noreply.github.com>
2022-10-11 10:58:10 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { Container } from "@material-ui/core";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+import PostDetails from "./components/PostDetails/PostDetails";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
+import CreatorOrTag from "./components/CreatorOrTag/CreatorOrTag";
+
+const alanKey =
+  "5ec686f5cc009c35322e56d463fb664c2e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === "testCommand") {
+          alert("Am good . Thank you.");
+        }
+      },
+    });
+  }, []);
+
+  const user = JSON.parse(localStorage.getItem("profile"));
+
+  return (
+    <BrowserRouter>
+      <Container maxWidth="xl">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={() => <Redirect to="/posts" />} />
+          <Route path="/posts" exact component={Home} />
+          <Route path="/posts/search" exact component={Home} />
+          <Route path="/posts/:id" exact component={PostDetails} />
+          <Route
+            path={["/creators/:name", "/tags/:name"]}
+            component={CreatorOrTag}
+          />
+          <Route
+            path="/auth"
+            exact
+            component={() => (!user ? <Auth /> : <Redirect to="/posts" />)}
+          />
+        </Switch>
+      </Container>
+    </BrowserRouter>
+  );
+};
+
+export default App;
